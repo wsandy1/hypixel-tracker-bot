40fd8d90e49ddaa376eeaebc8c68df8f148fa30c
William <mrsandywilly@protonmail.com>
2022-01-28 20:14:22 +0000

    fuck

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ for (ign of igns) {
         });
 }
 
-
 function update() {
+    console.log('update')
+    var usersref = users
     for (user of users) {
         axios.get(`https://api.hypixel.net/status?uuid=${user.uuid}`, {
             headers: {
@@ -30,36 +31,49 @@ function update() {
         })
             .then(res => {
                 user.online = res.data.session.online;
+                console.log(user)
             });
     }
 
-    var embed = {
-        title: 'Hypixel Tracking',
-        description: ''
-    };
-
-    for (user of users) {
-        if (user.online == true) {
-            embed.description += `\n<:green_circle:936683622688243752> ${user.name} - *online*`;
-        } else {
-            continue;
+    var refresh = false;
+    users.forEach((user, index) => {
+        // console.log("prev" + usersref[index].online);
+        // console.log("now" + user.online);
+        if (user.online != usersref[index].online) {
+            console.log('needrefresh')
+            refresh = true;
         }
-    }
+    });
 
-    for (user of users) {
-        if (user.online == false) {
-            embed.description += `\n<:red_circle:936684130895282176> ${user.name} - *offline*`;
-        } else {
-            continue;
+    if (refresh) {
+        var embed = {
+            title: 'Hypixel Tracking',
+            description: ''
+        };
+
+        for (user of users) {
+            if (user.online == true) {
+                embed.description += `\n<:green_circle:936683622688243752> ${user.name} - *online*`;
+            } else {
+                continue;
+            }
         }
-    }
 
-    client.channels.cache.get('936524453582618644').send({ embeds: [embed] });
+        for (user of users) {
+            if (user.online == false) {
+                embed.description += `\n<:red_circle:936684130895282176> ${user.name} - *offline*`;
+            } else {
+                continue;
+            }
+        }
+
+        client.channels.cache.get('936524453582618644').send({ embeds: [embed] });
+    }
 }
 
 client.once('ready', () => {
 	console.log('Ready!');
-    var interval = setInterval(update, 60000);
+    var interval = setInterval(update, 10000);
 });
 
 client.login(token);
